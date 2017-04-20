3c12c11d15094e14286d3a52c64384e2cb6ce5b6
tuanhoang pham <hoangpham1107@gmail.com>
2017-04-20 16:44:25 +0800

    updating blocking hour

--- website_blocker.py
+++ website_blocker.py
@@ -8,7 +8,7 @@ website_list=["www.facebook.com","facebook.com"]
 
 #start for while loop
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
         with open(hosts_path,'r+') as file:
             content=file.read()
             print content
