48f9b9f9502eef2617515644ef18ca827c02af43
tuanhoang pham <hoangpham1107@gmail.com>
2017-04-20 16:20:03 +0800

    add logic to block website

--- website_blocker.py
+++ website_blocker.py
@@ -8,9 +8,18 @@ website_list=["www.facebook.com","facebook.com"]
 
 #start for while loop
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            print content
+            #iterate in array
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
         print "Working hours"
     else:
         print "sleep hour"
 
-    time.sleep(100)
+    time.sleep(5)
