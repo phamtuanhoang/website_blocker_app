c07011f53d28e853fc9d5d48073a5066af181461
tuanhoang pham <hoangpham1107@gmail.com>
2017-04-20 16:41:57 +0800

    open host file truncate necessary lines

--- website_blocker.py
+++ website_blocker.py
@@ -8,7 +8,7 @@ website_list=["www.facebook.com","facebook.com"]
 
 #start for while loop
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         with open(hosts_path,'r+') as file:
             content=file.read()
             print content
@@ -20,6 +20,13 @@ while True:
                     file.write(redirect + " " + website + "\n")
         print "Working hours"
     else:
+        with open(hosts_path, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print "sleep hour"
 
     time.sleep(5)
